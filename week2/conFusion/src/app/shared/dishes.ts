4dff490ee0676cacb963137a2a446e77970d1b9d
leehsihau <34845784+leehsihau@users.noreply.github.com>
2021-05-27 10:26:43 -0500

    font-awesome path needs to be set. (Pay attention to _variables.scss)

--- week2/conFusion/src/app/shared/dishes.ts
+++ week2/conFusion/src/app/shared/dishes.ts
@@ -14,31 +14,31 @@ export const DISHES: Dish[] =[
         comments: [
             {
                 rating: 5,
-                comment: 'Imagine all the eatables, living in conFusion!',
+                comment: 'Imagine all the eatables, living in conFusion!\n',
                 author: 'John Lemon',
                 date: '2012-10-16T17:57:28.556094Z'
             },
             {
                 rating: 4,
-                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!\n',
                 author: 'Paul McVites',
                 date: '2014-09-05T17:57:28.556094Z'
             },
             {
                 rating: 3,
-                comment: 'Eat it, just eat it!',
+                comment: 'Eat it, just eat it!\n',
                 author: 'Michael Jaikishan',
                 date: '2015-02-13T17:57:28.556094Z'
             },
             {
                 rating: 4,
-                comment: 'Ultimate, Reaching for the stars!',
+                comment: 'Ultimate, Reaching for the stars!\n',
                 author: 'Ringo Starry',
                 date: '2013-12-02T17:57:28.556094Z'
             },
             {
                 rating: 2,
-                comment: 'It\'s your birthday, we\'re gonna party!',
+                comment: 'It\'s your birthday, we\'re gonna party!\n',
                 author: '25 Cent',
                 date: '2011-12-02T17:57:28.556094Z'
             }
@@ -56,31 +56,31 @@ export const DISHES: Dish[] =[
         comments: [
             {
                 rating: 5,
-                comment: 'Imagine all the eatables, living in conFusion!',
+                comment: 'Imagine all the eatables, living in conFusion!\n',
                 author: 'John Lemon',
                 date: '2012-10-16T17:57:28.556094Z'
             },
             {
                 rating: 4,
-                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!\n',
                 author: 'Paul McVites',
                 date: '2014-09-05T17:57:28.556094Z'
             },
             {
                 rating: 3,
-                comment: 'Eat it, just eat it!',
+                comment: 'Eat it, just eat it!\n',
                 author: 'Michael Jaikishan',
                 date: '2015-02-13T17:57:28.556094Z'
             },
             {
                 rating: 4,
-                comment: 'Ultimate, Reaching for the stars!',
+                comment: 'Ultimate, Reaching for the stars!\n',
                 author: 'Ringo Starry',
                 date: '2013-12-02T17:57:28.556094Z'
             },
             {
                 rating: 2,
-                comment: 'It\'s your birthday, we\'re gonna party!',
+                comment: 'It\'s your birthday, we\'re gonna party!\n',
                 author: '25 Cent',
                 date: '2011-12-02T17:57:28.556094Z'
             }
@@ -98,31 +98,31 @@ export const DISHES: Dish[] =[
         comments: [
             {
                 rating: 5,
-                comment: 'Imagine all the eatables, living in conFusion!',
+                comment: 'Imagine all the eatables, living in conFusion!\n',
                 author: 'John Lemon',
                 date: '2012-10-16T17:57:28.556094Z'
             },
             {
                 rating: 4,
-                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!\n',
                 author: 'Paul McVites',
                 date: '2014-09-05T17:57:28.556094Z'
             },
             {
                 rating: 3,
-                comment: 'Eat it, just eat it!',
+                comment: 'Eat it, just eat it!\n',
                 author: 'Michael Jaikishan',
                 date: '2015-02-13T17:57:28.556094Z'
             },
             {
                 rating: 4,
-                comment: 'Ultimate, Reaching for the stars!',
+                comment: 'Ultimate, Reaching for the stars!\n',
                 author: 'Ringo Starry',
                 date: '2013-12-02T17:57:28.556094Z'
             },
             {
                 rating: 2,
-                comment: 'It\'s your birthday, we\'re gonna party!',
+                comment: 'It\'s your birthday, we\'re gonna party!\n',
                 author: '25 Cent',
                 date: '2011-12-02T17:57:28.556094Z'
             }
@@ -140,31 +140,31 @@ export const DISHES: Dish[] =[
         comments: [
             {
                 rating: 5,
-                comment: 'Imagine all the eatables, living in conFusion!',
+                comment: 'Imagine all the eatables, living in conFusion!\n',
                 author: 'John Lemon',
                 date: '2012-10-16T17:57:28.556094Z'
             },
             {
                 rating: 4,
-                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!\n',
                 author: 'Paul McVites',
                 date: '2014-09-05T17:57:28.556094Z'
             },
             {
                 rating: 3,
-                comment: 'Eat it, just eat it!',
+                comment: 'Eat it, just eat it!\n',
                 author: 'Michael Jaikishan',
                 date: '2015-02-13T17:57:28.556094Z'
             },
             {
                 rating: 4,
-                comment: 'Ultimate, Reaching for the stars!',
+                comment: 'Ultimate, Reaching for the stars!\n',
                 author: 'Ringo Starry',
                 date: '2013-12-02T17:57:28.556094Z'
             },
             {
                 rating: 2,
-                comment: 'It\'s your birthday, we\'re gonna party!',
+                comment: 'It\'s your birthday, we\'re gonna party!\n',
                 author: '25 Cent',
                 date: '2011-12-02T17:57:28.556094Z'
             }
