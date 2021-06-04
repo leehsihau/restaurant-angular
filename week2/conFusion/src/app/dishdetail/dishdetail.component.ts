0d59d064439204f061a335c05b4518007733d5b7
leehsihau <34845784+leehsihau@users.noreply.github.com>
2021-06-04 13:31:16 -0500

    Promise part1

--- week2/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ week2/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -15,7 +15,7 @@ export class DishdetailComponent implements OnInit {
 
   ngOnInit(): void {
     let id = this.routeService.snapshot.params['id'];
-    this.dish = this.dishService.getDish(id);
+    this.dishService.getDish(id).then((dish)=>this.dish=dish);
   }
 
   goBack(): void{
